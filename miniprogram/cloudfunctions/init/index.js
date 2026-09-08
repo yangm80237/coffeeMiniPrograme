12eb50c85c90ec44607fc86a27b18b93d607fdf5
yangm80237 <yangm80237@qq.com>
2026-09-08 21:13:26 +0800

    fix(④): init种子config默认模型更新为实际开通的版本(2-0-lite/seedream-5-0)

--- miniprogram/cloudfunctions/init/index.js
+++ miniprogram/cloudfunctions/init/index.js
@@ -35,7 +35,7 @@ exports.main = async (event) => {
   const ark = await db.collection('config').doc('ark').get().catch(() => null);
   if (!ark || !ark.data) {
     await db.collection('config').add({
-      data: { _id: 'ark', modelVision: 'doubao-seed-1.6', modelImage: 'doubao-seedream-4.0' },
+      data: { _id: 'ark', modelVision: 'doubao-seed-2-0-lite-260428', modelImage: 'doubao-seedream-5-0-260128' },
     });
   }
   return { brandsInserted, flavorsInserted };
